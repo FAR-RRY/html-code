a188f4693e7d8b721ef6ed22b2641a1d1405037c
Fariha Khan <farihafirozkhan@gmail.com>
2023-07-21 04:10:59 +0530

    YES

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+console.log('Hello Javascript!');
+num=35;
+console.log(num);
+message="good";
+console.log(message);
+console.log(typeof num);
+age = 30;
+if(age>=18){
+    console.log('eligible for DL');
+} else {
+    console.log('not eligible');
+}
